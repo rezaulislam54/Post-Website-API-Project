89727f5bb356c8c59108a5b445d74c2880f21ad3
rezaulislam54 <rezaulislam5490@gmail.com>
2024-08-24 21:16:53 +0600

    posts and latest post section complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,82 @@
+const loadDiscus = async (searchText) =>{
+    const  rev = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category?=${searchText}`);
+    const allPost = await rev.json();
+    const totalPost = allPost.posts
+    loadDiscusDisplay(totalPost);
+}
+
+const loadDiscusDisplay = (posts) =>{
+    const discusContainer = document.getElementById('discus-container');
+    posts.forEach(data => {
+    // console.log(data);
+    const div = document.createElement('div');
+    div.classList=`m-5`;
+    div.innerHTML=` <div class="bg-gray-300 border border-black border-opacity-25 rounded-lg shadow-lg p-6">
+                        <div class="flex gap-x-5 ">
+                            <div class=" relative flex-shrink-0">
+                                <div class="absolute -top-1 left-12 w-3 h-3 border rounded-full bg-green-700 border-gray-300 "></div>
+                                <img src="${data.image}" alt="" class="w-14 h-14 border rounded-lg  dark:border-gray-300">
+		                    </div>
+                            <div class="">
+                                <span class="text-[16px] font-medium mr-3">#${data.category}</span>
+                                <span>Author: ${data.author.name}</span>
+                                <h1 class="text-2xl my-1 font-semibold">${data.title}</h1>
+                                <p class="opacity-70 my-3">${data.description} consectetur adipisi itaque laudantium explicabo</p>
+                                <div class="flex justify-between items-center mr-2">
+                                    <div class="space-x-2">
+                                        <i class="fa-regular fa-message"></i> <span>${data.comment_count}</span>
+                                        <i class="fa-regular fa-eye pl-5"></i><span>${data.view_count}</span>
+                                        <i class="pl-5 fa-regular fa-clock"></i> <span>${data.posted_time} min</span>
+                                    </div>
+                                    <button onclick="handleRedPost('${data.id}'),incrementValue()" class="bg-green-500 w-9 h-9 rounded-full text-white"><i class="fa-solid fa-envelope-open"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+`;
+    discusContainer.appendChild(div);
+   })
+}
+loadDiscus();
+
+// hendle search Field
+const handelSearchField = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadDiscus(searchText);
+}
+
+
+// handle Details Vew
+const handleRedPost = async(id) => {
+    console.log('clicked the button',id);
+    const respons = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+    const data = await respons.json();
+    const allData = data.posts;
+    const allPost = allData.find(p => p.id === +id);
+    showRedPost(allPost);  
+}
+
+// Red Post Value Increment 
+ const valueDisplay = document.getElementById('redCount');
+  let value = 0;
+   function incrementValue() {
+         value += 1; 
+        valueDisplay.textContent = value;
+ }
+
+const showRedPost = (data) =>{
+    const showContainer = document.getElementById('showContainer');
+    const div = document.createElement('div');
+    div.classList=`flex justify-between bg-white rounded-lg border shadow-md p-4 my-5`;
+    div.innerHTML = `
+         <h2 class="text-xl font-medium">${data.title}</h2>
+         <h1> <i class="fa-regular fa-eye"><span>${data.view_count}</span></i></h1>`;
+         showContainer.appendChild(div);
+}
+
+
 const loadLatestPost = async () => {
     const revs = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await revs.json();
@@ -6,7 +85,7 @@ const loadLatestPost = async () => {
 
 
 const postDisplay = (posts) =>{
-    console.log(posts);
+    // console.log(posts);
     const postContainer = document.getElementById('post-container');
     posts.forEach(element => {
        const div  = document.createElement('div');
